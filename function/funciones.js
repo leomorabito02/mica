20166577344a704037862d9f288b3742eed1109c
Leonardo Morabito <leonardomorabito02@gmail.com>
2022-07-12 21:55:04 -0300

    listo

--- function/funciones.js
+++ function/funciones.js
@@ -1,3 +1,6 @@
 function redireccion() {
-alert("a");
+window.open("https://wa.me/3513941076?text=Hola mi podrido <3");
+}
+function redireccion1() {
+   alert("Vas a superar todo ❤️, sos fuerte\nMejor dicho, vamos a superarlo los dos❤️");
 }
